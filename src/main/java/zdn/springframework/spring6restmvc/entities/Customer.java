683f76dfc87b1ede1cf6b22555905b26f18f9084
Ismail Ozden <ismailozden.de@gmail.com>
2023-10-02 11:35:57 +0200

    Create Beer and Customer entities.

---
+++ src/main/java/zdn/springframework/spring6restmvc/entities/Customer.java
@@ -0,0 +1,29 @@
+package zdn.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Integer version;
+    private String customerName;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
