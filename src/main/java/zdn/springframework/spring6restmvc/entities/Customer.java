eebf5e793ad1977afd051543b4a8bf9ce07fec1d
Ismail Ozden <ismailozden.de@gmail.com>
2023-10-04 14:18:42 +0200

    Add new column to customer entity and its sql scripts

--- src/main/java/zdn/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/zdn/springframework/spring6restmvc/entities/Customer.java
@@ -24,6 +24,9 @@ public class Customer {
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
+    @Column(length = 255)
+    private String email;
+
     @Version
     private Integer version;
     private String customerName;
